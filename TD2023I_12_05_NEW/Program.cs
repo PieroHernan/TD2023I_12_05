6d5e8745f844a8fb67164472ed979f29be850ff0
lenovo <PIEROHERNAN10@gmail.com>
2023-05-12 19:43:54 -0500

    Agregar linea de codigo

--- TD2023I_12_05_NEW/Program.cs
+++ TD2023I_12_05_NEW/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye, World!");
+            Console.WriteLine("Hola, Mundo!");
         }
     }
 }
