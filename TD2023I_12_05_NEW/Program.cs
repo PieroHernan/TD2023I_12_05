8d1a02b9ace3b89805f86eb520341720953865ec
lenovo <PIEROHERNAN10@gmail.com>
2023-05-12 19:01:36 -0500

    Agregar archivos de proyecto.

---
+++ TD2023I_12_05_NEW/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TD2023I_12_05_NEW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TD2023I_12_05_NEW/Prueba.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD2023I_12_05_NEW
+{
+    internal class Prueba
+    {
+    }
+}
